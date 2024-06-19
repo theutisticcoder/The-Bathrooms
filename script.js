22780c6c4092fb9421adde18632b7d4d13c8d10f
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-06-19 16:48:12 -0400

    new start

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-var toilet = document.getElementById("toilet");
-var place;
 
-var number;
-var coordinates = [
+this.coordinates = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -48,288 +45,48 @@ var coordinates = [
   [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
-
-var monster = document.createElement("img");
-monster.src = "t.png";
-monster.id = "monster";
-monster.style.height = "500px";
-monster.style.width = "500px";
-monster.style.position = "absolute";
-monster.style.transformStyle = "preserve-3d";
-document.getElementById("maze").appendChild(monster);
-for (var x = 0; x < coordinates[0].length; x++) {
-
-  for (var z = 0; z < coordinates.length; z++) {
-    if (coordinates[z][x] > 0) {
-      toilet = toilet.cloneNode(true);
-      toilet.style.position = "absolute";
-      toilet.style.height = "500px";
-      toilet.hidden = false;
-      toilet.style.transformStyle = "preserve-3d";
-      toilet.style.width = "500px";
-      toilet.style.transformOrigin = "0px 0px 0px";
-      toilet.style.perspective = "6000px";
-      toilet.style.transform =
-        "translate3d(" + (x * 500) + "px, 0px, " + (z * -500) + "px)";
-      document.getElementById("maze").appendChild(toilet);
-    }
-  }
-}
-maze.style.rotate = "1 0 0 180deg";
-maze.style.translate = "0px 1000px -2000px";
-function spawn() {
-	document.getElementById("monster").style.transform =
-      "translate3d(" +
-      Math.floor(Math.random() * 20) * 500 +
-      "px,  0px, " +
-      Math.floor(Math.random() * 10) * 500 +
-      "px)";
-  setTimeout(() => {
-    
-
-    spawn();
-  }, 10000);
-}
-spawn();
-var dmatrix, tmatrix;
-function isCollide(a, b) {
-  dmatrix = new WebKitCSSMatrix(getComputedStyle(a).transform);
-  tmatrix = new WebKitCSSMatrix(getComputedStyle(b).transform);
-  if (tmatrix.m41 >= dmatrix.m41 && tmatrix.m41 >= (dmatrix.m41 + 500) && tmatrix.m43 >= dmatrix.m43 && tmatrix.m43 >= (dmatrix.m43 + 500)) {
-		console.log(tmatrix.m41);
-		console.log(tmatrix.m43);
-    return true;
-  } else return false;
-}
-var a = 500;
-var b = 0;
-var y = 0;
-var t1 = 0;
-var c = 1500;
-var trues = 0;
-document.getElementById("dummy").style.transform =
-  "translate3d( " + c + "px, " + y + "px, " + a + "px)";
-document.getElementById("maze").style.transform =
-  "translate3d(" + c + "px, " + y + "px, " + a + "px)";
-var rotate = 0;
-var toilets = Array.from(document.getElementsByClassName("toilet"));
-document.addEventListener("keydown", (e) => {
-  trues = 0;
-  document.body.style.perspective = "5000px";
-  if (e.key == "ArrowUp") {
-    e.preventDefault();
-    a -= 50;
-    document.getElementById("maze").transformOrigin =
-      c + "px " + y + "px " + a + "px";
-    document.getElementById("dummy").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-    document.getElementById("maze").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-    for (var i = 0; i < toilets.length; i++) {
-      console.log(isCollide(document.getElementById("dummy"), toilets[i]));
-
-      if (isCollide(document.getElementById("dummy"), toilets[i]) == true) {
-        trues++;
-      }
-    }
-    if (trues != 0) 
-	{
-    return;
-    }
-  } else if (e.key == "ArrowDown") {
-    e.preventDefault();
-a += 50
-    document.getElementById("maze").transformOrigin =
-      c + "px " + y + "px " + a + "px";
-    document.getElementById("dummy").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-    document.getElementById("maze").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-    for (var i = 0; i < toilets.length; i++) {
-      console.log(isCollide(document.getElementById("dummy"), toilets[i]));
-      if (isCollide(document.getElementById("dummy"), toilets[i]) == true) {
-        trues++;
-      }
-    }
-    if (trues != 0) {
-     return;
-    }
-  }
-  if (e.key == "a") {
-    e.preventDefault();
-    rotate += 10;
-    document.getElementById("maze").transformOrigin =
-      c + "px " + y + "px " + a + "px";
-    document.getElementById("dummy").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-    document.getElementById("maze").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-  }
-  if (e.key == "d") {
-    e.preventDefault();
-    rotate -= 10;
-    document.getElementById("maze").transformOrigin =
-      c + "px " + y + "px " + a + "px";
-    document.getElementById("dummy").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-    document.getElementById("maze").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
+var canvas = document.querySelector("canvas");
+class ray{
   
-  } else if (e.key == "ArrowLeft") {
-    e.preventDefault();
-    c += 50;
-    document.getElementById("maze").transformOrigin =
-      c + "px " + y + "px " + a + "px";
-    document.getElementById("dummy").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-    document.getElementById("maze").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-    for (var i = 0; i < toilets.length; i++) {
-      console.log(isCollide(document.getElementById("dummy"), toilets[i]));
-
-      if (isCollide(document.getElementById("dummy"), toilets[i]) == true) {
-        trues++;
-      }
-    }
-    if (trues != 0) {
-      return;
-    }
-  } else if (e.key == "ArrowRight") {
-    e.preventDefault();
-    c -= 50
-    document.getElementById("maze").transformOrigin =
-      c + "px " + y + "px " + a + "px";
-    document.getElementById("dummy").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-    document.getElementById("maze").style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-    for (var i = 0; i < toilets.length; i++) {
-      console.log(isCollide(document.getElementById("dummy"), toilets[i]));
-
-      if (isCollide(document.getElementById("dummy"), toilets[i]) == true) {
-        trues++;
-      }
-    }
-    if (trues != 0) {
-     return;
+  drawWallSlice(i, wallHeight, sliceWidth) {
+    for (let j = 0; j < wallHeight; j++) {
+        let yPosition = Math.floor(300 - wallHeight / 2 + j);
+        this.ctx.fillStyle = `rgb(180, 0, 180)`;
+        this.ctx.fillRect(i * sliceWidth, yPosition, sliceWidth, 1);
     }
   }
-  if (e.key == " ") {
-    e.preventDefault();
-    place = document.createElement("div");
-    place.style.backgroundImage = "url(toiletpaper.png)";
-    place.style.position = "absolute";
-    place.style.height = "100px";
-    place.style.transformStyle = "preserve-3d";
-    place.style.width = "100px";
-    place.style.transform =
-      "translate3d(" +
-      c +
-      "px, " +
-      y +
-      "px, " +
-      a +
-      "px) perspective(" +
-      (a * 2) +
-      "px) rotateY(" +
-      rotate +
-      "deg)";
-    document.getElementById("maze").appendChild(place);
+   raycast() {
+    const rays = 200;
+    const screenWidth = 800;
+    const sliceWidth = screenWidth / rays;
+    const angleStep = this.player.fov / rays;
+  
+    // Walls
+    for (let i = 0; i < rays; i++) {
+        const rayAngle = this.player.angle - (this.player.fov / 2) + i * angleStep;
+        const { distance, wallHeight } = this.castRay(rayAngle);
+        this.drawWallSlice(i, wallHeight, sliceWidth);
+    }
   }
-});
+   castRay(rayAngle) {
+    let x = this.player.x;
+    let y = this.player.y;
+    let dx = Math.cos(rayAngle);
+    let dy = Math.sin(rayAngle);
+  
+    // Increment x and y until we hit a wall
+    let i = 0;
+    while (this.map[Math.floor(y)][Math.floor(x)] === 0) {
+        x += dx * 0.1;
+        y += dy * 0.1;
+        i++;
+        if (i > 400) break;  // Prevent infinite loops
+    }
+}
+}
+
+  const distance = Math.sqrt((x - this.player.x) ** 2 + (y - this.player.y) ** 2);
+  const wallHeight = 300 / distance;
+
+  return { distance, wallHeight };
+
